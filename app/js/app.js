1566dbff8436871fec4f79b27643e9a92ada88c4
isen <isen@ISENMIC-N39MQDE.home>
2013-05-19 19:44:19 +0200

    add main view

--- app/js/app.js
+++ app/js/app.js
@@ -6,5 +6,6 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+     $routeProvider.when('/main_view', {templateUrl: 'partials/main_view.html', controller: 'MyCtrl2'});
+    $routeProvider.otherwise({redirectTo: '/main_view'});
   }]);
