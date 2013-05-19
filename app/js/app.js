de37410e76e47ba5697aff7b45e5447818dec981
mvand <mvand@mvand-laptop.home>
2013-05-19 20:31:45 +0200

    ctrl

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('smwApp', ['ngResource','smwApp.filters', 'smwApp.services', 'smwApp.directives', 'smwApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MainCtrl'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-     $routeProvider.when('/main_view', {templateUrl: 'partials/main_view.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html'});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html'});
+     $routeProvider.when('/main_view', {templateUrl: 'partials/main_view.html', controller: 'MainCtrl'});
     $routeProvider.otherwise({redirectTo: '/main_view'});
   }]);
