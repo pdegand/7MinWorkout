796c56a19d007897608dd965d4ef9e5bd8746983
Pierre Degand <pierre.degand@gmail.com>
2013-05-19 20:25:08 +0200

    Reformat

--- app/js/main_view.js
+++ app/js/main_view.js
@@ -1,54 +1,56 @@
-$(document).ready(function(){
-    
-setTimeout(cycler,1000);
-    
-}) 
+$(document).ready(function() {
 
-var steps = ["Push up", "Break","coucou", "Break","prout", "Break", "End"];
+    setTimeout(cycler, 1000);
+
+})
+
+var steps = ["Push up", "Break", "coucou", "Break", "prout", "Break", "End"];
 var cursor = 1;
 
-var stepsView =["#firstStep","#secondStep"];
-var stepsNextView =["#firstNextStep","#secondNextStep"];
+var stepsView = ["#firstStep", "#secondStep"];
+var stepsNextView = ["#firstNextStep", "#secondNextStep"];
 
-function cycler(){
-    
-cycle = $('#steps').cycle({ 
-    fx:      'scrollLeft', 
-    timeout:  0,
-    after:update
-});
+function cycler() {
 
-cycleNext = $('#next_steps').cycle({ 
-    fx:      'scrollLeft', 
-    timeout:  0,
-});
-init();
-} 
+    cycle = $('#steps').cycle({
+        fx: 'scrollLeft',
+        timeout: 0,
+        after: update
+    });
 
-function next(){
-    $('#steps').cycle("resume",true);
-    $('#next_steps').cycle("resume",true);
- //   update();
+    cycleNext = $('#next_steps').cycle({
+        fx: 'scrollLeft',
+        timeout: 0,
+    });
+    init();
 }
 
-function update(){
-    if(cursor < steps.length)
-        $(stepsView[cursor%2]).html(steps[cursor]);
-    else $(stepsView[cursor%2]).html("");
-    
-    if(cursor+1 < steps.length)
-        {$(stepsNextView[cursor%2]).html(steps[cursor+1]);
-        }
-    else 
-        {
-            $(stepsNextView[cursor%2]).html("");
-        }
+function next() {
+    $('#steps').cycle("resume", true);
+    $('#next_steps').cycle("resume", true);
+    //   update();
+}
+
+function update() {
+    if (cursor < steps.length)
+        $(stepsView[cursor % 2]).html(steps[cursor]);
+    else
+        $(stepsView[cursor % 2]).html("");
+
+    if (cursor + 1 < steps.length)
+    {
+        $(stepsNextView[cursor % 2]).html(steps[cursor + 1]);
+    }
+    else
+    {
+        $(stepsNextView[cursor % 2]).html("");
+    }
     cursor++;
 }
 
-function init(){
+function init() {
     $(stepsView[0]).html(steps[0])
     $(stepsView[1]).html(steps[1])
     $(stepsNextView[0]).html(steps[1])
-    $(stepsNextView[1]).html(steps[2]) 
+    $(stepsNextView[1]).html(steps[2])
 }
