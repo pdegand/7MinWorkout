a1a6bcc46b92ff12b86c917653a5b76b98e4fa64
mvand <mvand@mvand-laptop.home>
2013-05-20 12:40:46 +0200

    lang support

--- app/js/services.js
+++ app/js/services.js
@@ -5,6 +5,6 @@
 angular.module('smwApp.services', []).
   value('version', '0.1').
   factory('Steps',function($resource, $locale) {
-      console.log($locale.id);
+      console.log($locale.id + " " + navigator.language);
       return $resource('data/exercises.json');
   });
