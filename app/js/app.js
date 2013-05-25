79ac51becc45901f6d8159b4c2ba45b2e3a36f9a
isen <isen@ISENMIC-N39MQDE.home>
2013-05-25 16:33:14 +0200

    bug switching page and reset param fixed

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('smwApp', ['ngResource', 'smwApp.filters', 'smwApp.services', 'smwApp.directives', 'smwApp.controllers','l10n','l10n-tools','smw-l10n']).
         config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/home', {templateUrl: 'partials/home.html'});
+        $routeProvider.when('/home', {templateUrl: 'partials/smwApp.html', controller: 'MainCtrl'});
         $routeProvider.when('/infos', {templateUrl: 'partials/infos_en-us.html'});
         $routeProvider.when('/smw', {templateUrl: 'partials/smwApp.html', controller: 'MainCtrl'});
         $routeProvider.when('/exercises', {templateUrl: 'partials/exercises.html', controller: 'ExercisesCtrl'});
